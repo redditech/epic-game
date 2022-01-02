3f2cdc1168749d2b5065049f8045d9d095ba9a19
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-02 17:39:42 +1000

    add initial characters

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,45 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("This is my game contract. Nice!");
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    // An array to help hold default data for all characters
+    // Helpful when we mint new characters
+    // to know their HP, AD, etc.
+    CharacterAttributes[] defaultCharacters;
+
+    // Data passed into the contract when it's first created initializing the characters.
+    // We're going to actually pass these values in from run.js
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg
+    ) 
+    {
+        // Loop through all the characters and save their values in our contract
+        // so that we can use them later when we mint our NFTs.
+        for(uint i = 0; i < characterNames.length; i+=1) {
+            defaultCharacters.push(CharacterAttributes({
+                characterIndex: i,
+                name: characterNames[i],
+                imageURI: characterImageURIs[i],
+                hp: characterHp[i],
+                maxHp: characterHp[i],
+                attackDamage: characterAttackDmg[i]
+            }));
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done initializing %s w/HP %s, img %s", c.name, c.hp, c.imageURI);
+        }
+
     }
 }
