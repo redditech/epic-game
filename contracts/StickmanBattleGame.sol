fbe10608a23266991f7e56f44e5a42e93a86edbf
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-03 14:38:52 +1000

    add traits, deploy to rinkeby

---
+++ contracts/StickmanBattleGame.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+// NFT contract to inherit from
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+// Helper functions OpenZeppelin provides
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "hardhat/console.sol";
+
+// Let contract inherit from ERC721 - the standard NFT contract
+contract StickmanBattleGame is ERC721 {
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        string weapon;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    // The tokenId is the NFT's unique identifier, it's justa number that goes
+    // 0, 1, 2, 3 etc
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // An array to help hold default data for all characters
+    // Helpful when we mint new characters
+    // to know their HP, AD, etc.
+    CharacterAttributes[] defaultCharacters;
+
+    // Create a mapping from the nft's tokenId => the NFT attributes
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // Create a mapping from an address => the NFT's tokenId.
+    // This is an easy way to store the owner of the NFT and reference it later
+    mapping(address => uint256) public nftHolders;
+
+    // Data passed into the contract when it's first created initializing the characters.
+    // We're going to actually pass these values in from run.js
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        string[] memory characterWeapons,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    )
+        // Below this, the name and symbol for our tokenURI(tokenId);
+        ERC721("StickmanBattleGame", "SMBG")
+    {
+        // Loop through all the characters and save their values in our contract
+        // so that we can use them later when we mint our NFTs.
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    weapon: characterWeapons[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s with w/HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+            console.log("%s is armed with weapon: %s", c.name, c.weapon);
+        }
+
+        // increment tokenIds here so that the first NFT has an ID of 1.
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+        string memory weapon = charAttributes.weapon;
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                " -- NFT #: ",
+                Strings.toString(_tokenId),
+                '", "description": "This is an NFT that lets people play in the game Stickman Battles!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Preferred Weapon", "value" : "',
+                weapon,'"',
+                '}, {"trait_type": "Health Points", "value" :',
+                strHp,
+                ', "max_value":',
+                strMaxHp,
+                '}, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                "} ]}"
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        return output;
+    }
+
+    // Users hit this function to get their NFT based on the
+    // characterId they send in
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        // Get current tokenId (starts at 1 since we incremented in the constructor)
+        uint256 newItemId = _tokenIds.current();
+
+        // assign the tokenId to the caller's wallet address
+        _safeMint(msg.sender, newItemId);
+
+        // Map the tokenId => this character's attributes.
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            weapon: defaultCharacters[_characterIndex].weapon,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Minted NFT w/tokenId %s and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        // Keep an easy way to see who owns what NFT
+        nftHolders[msg.sender] = newItemId;
+
+        // Increment the tokenId for the next person that uses it.
+        _tokenIds.increment();
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,14 +14,22 @@
         console.log("Contract deployed to: ", gameContract.address);
 
         let txn;
-        // We only have three characters,
-        // an NFT w/the character at index 2 of our array
+        txn = await gameContract.mintCharacterNFT(0);
+        await txn.wait;
+        console.log("Minted NFT #1");
+
+        txn = await gameContract.mintCharacterNFT(1);
+        await txn.wait;
+        console.log("Minted NFT #2");
+
         txn = await gameContract.mintCharacterNFT(2);
         await txn.wait;
-
-        // Get the value of the NFT's URI
-        let returnedTokenUri = await gameContract.tokenURI(1);
-        console.log("Token URI:", returnedTokenUri);
+        console.log("Minted NFT #3")
+        ;
+        txn = await gameContract.mintCharacterNFT(1);
+        await txn.wait;
+        console.log("Minted NFT #4");
+        
         process.exit(0);
     } catch (error) {
         console.log(error);
