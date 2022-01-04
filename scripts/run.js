1015265c0ffa47792b7e149a0d33bf719b5b17f2
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-01-05 07:20:09 +1000

    change imageURLs to only have IPFS hash

--- scripts/run.js
+++ scripts/run.js
@@ -3,14 +3,14 @@
         const gameContractFactory = await hre.ethers.getContractFactory("StickmanBattleGame");
         const gameContract = await gameContractFactory.deploy(
             ["Red Shirt Hel", "Frosty Jack", "Blue Lee"],       // Names
-            ["https://cloudflare-ipfs.com/ipfs/QmYTW85JiTf75Yw2QNvCJP2Hyakg6fNTsyyhFzCdJDM52P", // Images
-                "https://cloudflare-ipfs.com/ipfs/QmdpwzcAZb4GanuEFMQgwX85FcCvh9X6aLHHaR2eFDHtAm",
-                "https://cloudflare-ipfs.com/ipfs/QmVuxzD9Vawj5Tr9dJLcxTUYMvZcJYRZsX7yCS2RtYRdBk"],
+            ["QmYTW85JiTf75Yw2QNvCJP2Hyakg6fNTsyyhFzCdJDM52P", // Images
+                "QmdpwzcAZb4GanuEFMQgwX85FcCvh9X6aLHHaR2eFDHtAm",
+                "QmVuxzD9Vawj5Tr9dJLcxTUYMvZcJYRZsX7yCS2RtYRdBk"],
             ["phaser", "katana", "staff"],
             [100, 200, 300],                    // HP values
             [100, 50, 25],                       // Attack damage values
             "Shady Moe", // Boss name
-            "https://cloudflare-ipfs.com/ipfs/QmVPxkugpFHXGuWpiUa3f8JY1sThht3A61J1aGWCkdbScZ", // Boss image
+            "QmVPxkugpFHXGuWpiUa3f8JY1sThht3A61J1aGWCkdbScZ", // Boss image
             "hook and claws", // boss weapon
             10000, // Boss hp
             50 // Boss attack damage
